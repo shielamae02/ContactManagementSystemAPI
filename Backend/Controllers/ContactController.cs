548d24ca249131955790040ae3e24db9e36dbe4b
Shiela <shllepon@gmail.com>
2023-08-10 17:47:21 +0800

    Added ContactController Documentation

--- Backend/Controllers/ContactController.cs
+++ Backend/Controllers/ContactController.cs
@@ -7,15 +7,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
 {
+
+    /// <summary>
+    /// Controller for managing contacts of an authenticated user.
+    /// </summary>
     [ApiController]
     [Route("api/contacts")]
     [Authorize]
-   
     public class ContactController : ControllerBase
     {
         private readonly IContactService _contactService;
         private readonly IUserService _userService;
         private readonly ILogger<ContactController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContactController"/> class.
+        /// </summary>
+        /// <param name="contactService">The service for managing contacts.</param>
+        /// <param name="logger">The logger instance for logging.</param>
+        /// <param name="userService">The service for user-related operations.</param>
         public ContactController(IContactService contactService, ILogger<ContactController> logger, IUserService userService)
         {
             _contactService = contactService ?? throw new ArgumentNullException(nameof(contactService));
@@ -23,7 +33,10 @@ namespace Backend.Controllers
             _userService = userService ?? throw new ArgumentNullException(nameof(userService));
         }
 
-
+        /// <summary>
+        /// Gets all contacts for the authenticated user.
+        /// </summary>
+        /// <returns>A list of contacts belonging to the user.</returns>
         [HttpGet]
         public async Task<IActionResult> GetContacts()
         {
@@ -49,6 +62,12 @@ namespace Backend.Controllers
             }
         }
 
+
+        /// <summary>
+        /// Gets a specific contact for the authenticated user.
+        /// </summary>
+        /// <param name="contactId">The ID of the contact to retrieve.</param>
+        /// <returns>The contact information if found, or an error response.</returns>
         [HttpGet("{contactId}")]
         public async Task<IActionResult> GetContact(int contactId)
         {
@@ -75,6 +94,12 @@ namespace Backend.Controllers
 
         }
 
+
+        /// <summary>
+        /// Adds a new contact for the authenticated user.
+        /// </summary>
+        /// <param name="newContact">The contact information to add.</param>
+        /// <returns>The added contact if successful, or an error response.</returns>
         [HttpPost]
         public async Task<IActionResult> AddContact(AddContactDto newContact)
         {
@@ -100,6 +125,12 @@ namespace Backend.Controllers
             }
         }
 
+
+        /// <summary>
+        /// Deletes a specific contact for the authenticated user.
+        /// </summary>
+        /// <param name="contactId">The ID of the contact to delete.</param>
+        /// <returns>A success message if the contact is deleted, or an error response.</returns>
         [HttpDelete("{contactId}")]
         public async Task<IActionResult> DeleteContact(int contactId)
         {
@@ -125,6 +156,12 @@ namespace Backend.Controllers
             }
         }
 
+        /// <summary>
+        /// Updates a specific contact for the authenticated user.
+        /// </summary>
+        /// <param name="contactId">The ID of the contact to update.</param>
+        /// <param name="updatedContact">The updated contact information.</param>
+        /// <returns>The updated contact information if successful, or an error response.</returns>
         [HttpPut("{contactId}")]
         public async Task<IActionResult> UpdateContact(int contactId, UpdateContactDto updatedContact)
         {
