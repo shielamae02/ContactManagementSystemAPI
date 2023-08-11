d3d359880371a2645256b6d513a0ea22ed8e1895
Shiela <shllepon@gmail.com>
2023-08-11 17:58:22 +0800

    Added AddressRepository

--- Backend/Repositories/Addresses/AddressRepository.cs
+++ Backend/Repositories/Addresses/AddressRepository.cs
@@ -18,6 +18,68 @@ namespace Backend.Repositories.Addresses
             _contactRepository = contactRepository ?? throw new ArgumentNullException(nameof(contactRepository));
         }
 
-        
+        public async Task<int> AddAddress(int contactId, Address newAddress)
+        {
+            _context.Addresses.Add(newAddress);
+            await _context.SaveChangesAsync();
+            return newAddress.Id;
+        }
+
+        public async Task<bool> DeleteAddress(int userId, int contactId, int addressId)
+        {
+            var contact = await _contactRepository.GetContact(userId, contactId);
+            if(contact is null)
+            {
+                throw new ContactNotFoundException("Contact not found.");
+            }
+
+            var db = _context.Addresses;
+            var address = await db.FirstOrDefaultAsync(c => c.ContactId == contact.Id && c.Id == addressId);
+            if (address is null)
+            {
+                return false;
+            }
+            db.Remove(address);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Address?> GetAddress(int userId, int contactId, int addressId)
+        {
+            var contact = await _contactRepository.GetContact(userId, contactId);
+            if (contact is null)
+            {
+                throw new ContactNotFoundException("Contact not found.");
+            }
+
+            return await _context.Addresses.FirstOrDefaultAsync(c => c.ContactId == contact.Id && c.Id == addressId);
+        }
+
+        public async Task<ICollection<Address>> GetAddresses(int userId, int contactId)
+        {
+            var contact = await _contactRepository.GetContact(userId, contactId);
+            if (contact is null)
+            {
+                throw new ContactNotFoundException("Contact not found.");
+            }
+            return await _context.Addresses.Where(c => c.ContactId == contact.Id).ToListAsync();
+        }
+
+        public async Task<bool> UpdateAddress(int contactId, Address updateAddress)
+        {
+            var address = await _context.Addresses.FirstOrDefaultAsync(c => c.Id == updateAddress.Id && c.ContactId == contactId);
+            if(address is null)
+            {
+                return false;
+            }
+
+            address.ContactId = updateAddress.ContactId;
+            address.Details = updateAddress.Details;
+            address.Label = updateAddress.Label;
+            address.UpdatedAt = updateAddress.UpdatedAt;
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
