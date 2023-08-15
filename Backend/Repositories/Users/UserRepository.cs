88e8a06d77d56b217a259e56f3d60ed8147c7ca3
Shiela <shllepon@gmail.com>
2023-08-15 14:54:02 +0800

    Add User Repository Documentation

--- Backend/Repositories/Users/UserRepository.cs
+++ Backend/Repositories/Users/UserRepository.cs
@@ -1,19 +1,28 @@
 ﻿using Backend.Data;
 using Backend.Entities;
-using Backend.Models.Auths;
 using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Repositories.Users
 {
+    /// <summary>
+    /// Repository for managing user data.
+    /// </summary>
     public class UserRepository : IUserRepository
     {
         private readonly DataContext _context;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserRepository"/> class.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public UserRepository(DataContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
+
+        /// <inheritdoc/>
         public async Task<int> AddUser(User newUser)
         {
             _context.Users.Add(newUser);
@@ -21,11 +30,12 @@ namespace Backend.Repositories.Users
             return newUser.Id;
         }
 
+        /// <inheritdoc/>
         public async Task<bool> DeleteUser(int id)
         {
             var db = _context.Users;
             var user = db.FirstOrDefault(c => c.Id == id);
-            if(user is null)
+            if (user is null)
             {
                 return false;
             }
@@ -35,17 +45,21 @@ namespace Backend.Repositories.Users
             return true;
         }
 
+        /// <inheritdoc/>
         public async Task<User?> GetUser(User user)
         {
             return await _context.Users.FirstOrDefaultAsync(c => c.EmailAddress == user.EmailAddress);
         }
 
+
+        /// <inheritdoc/>
         public async Task<User> GetUserById(int id)
         {
             var user = await _context.Users.FirstOrDefaultAsync(c => c.Id == id);
             return user!;
         }
 
+        /// <inheritdoc/>
         public async Task<bool> UpdateUser(User updatedUser)
         {
             var user = await _context.Users.FirstOrDefaultAsync(c => c.Id == updatedUser.Id);
@@ -57,7 +71,6 @@ namespace Backend.Repositories.Users
             user.FirstName = updatedUser.FirstName;
             user.LastName = updatedUser.LastName;
             user.UserName = updatedUser.UserName;
-            user.EmailAddress = updatedUser.EmailAddress;
             user.UpdatedAt = updatedUser.UpdatedAt;
 
             await _context.SaveChangesAsync();
