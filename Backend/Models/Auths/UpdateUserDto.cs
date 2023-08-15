eb6dc43596d8f8353caeab2e0dde93999dab2041
Shiela <shllepon@gmail.com>
2023-08-15 14:39:42 +0800

    Add UpdateUserDto

---
+++ Backend/Models/Auths/UpdateUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.Auths
+{
+    public class UpdateUserDto
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        [MinLength(3, ErrorMessage = "First name must be at least 3 characters long.")]
+        [MaxLength(50, ErrorMessage = "First name must not exceed 50 characters.")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [MinLength(3, ErrorMessage = "Last name must be at least 3 characters long.")]
+        [MaxLength(50, ErrorMessage = "Last name must not exceed 50 characters.")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Username is required.")]
+        [MinLength(3, ErrorMessage = "Username must be at least 3 characters long.")]
+        [MaxLength(50, ErrorMessage = "Username must not exceed 50 characters.")]
+        public string UserName { get; set; } = string.Empty;
+    }
+}
