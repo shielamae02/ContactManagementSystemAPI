cee534a83662630555a6aac97816b88306aad7fe
Shiela <shllepon@gmail.com>
2023-08-15 09:50:19 +0800

    Add AuthService Documentation

--- Backend/Services/Auths/IAuthService.cs
+++ Backend/Services/Auths/IAuthService.cs
@@ -3,9 +3,23 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Backend.Services.Auths
 {
+    /// <summary>
+    /// Provides methods for user authentication and registration.
+    /// </summary>
     public interface IAuthService
     {
+        /// <summary>
+        /// Authenticates a user's login request.
+        /// </summary>
+        /// <param name="request">The user's login information.</param>
+        /// <returns>An authenticated user data transfer object if login is successful, otherwise null.</returns>
         Task<AuthUserDto> AuthLogin(UserLoginDto request);
+
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <param name="request">The user's registration information.</param>
+        /// <returns>An authenticated user data transfer object if registration is successful, otherwise null.</returns>
         Task<AuthUserDto> AuthRegister(UserRegisterDto request);
     }
 }
