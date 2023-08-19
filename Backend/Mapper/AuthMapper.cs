2f21789c01ee7c3553c69a522d8c3a04dec00aa7
Shiela <shllepon@gmail.com>
2023-08-19 10:41:06 +0800

    Add initial frontend

--- Backend/Mapper/AuthMapper.cs
+++ Backend/Mapper/AuthMapper.cs
@@ -10,12 +10,27 @@ namespace Backend.Mapper
         {
             CreateMap<User, UserLoginDto>()
                 .ReverseMap();
+
             CreateMap<User, UserRegisterDto>()
-                .ReverseMap();
+                .ReverseMap()
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => CapitalizeFirstLetter(src.FirstName)))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => CapitalizeFirstLetter(src.LastName)));
+
+
             CreateMap<User, AuthUserDto>()
                 .ReverseMap();
             CreateMap<User, UpdateUserDto>()
                 .ReverseMap();
+
+        }
+
+        private string CapitalizeFirstLetter(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+            return char.ToUpper(input[0]) + input.Substring(1);
         }
     }
 }
