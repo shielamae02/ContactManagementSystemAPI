f1eee48ca6d420cef2a9153d052ed666207aeb8c
Shiela <shllepon@gmail.com>
2023-08-22 04:04:48 +0800

    Fix AddAddressDto Validation

--- Backend/Models/Addresses/AddAddressDto.cs
+++ Backend/Models/Addresses/AddAddressDto.cs
@@ -5,7 +5,7 @@ namespace Backend.Models.Addresses
     public class AddAddressDto
     {
         [Required(ErrorMessage = "Address details are required.")]
-        [MinLength(5, ErrorMessage = "Address details must be at least 5 characters long.")]
+        [MinLength(3, ErrorMessage = "Address details must be at least 3 characters long.")]
         [MaxLength(100, ErrorMessage = "Address label must not exceed 100 characters.")]
         public string Details { get; set; } = string.Empty;
 
