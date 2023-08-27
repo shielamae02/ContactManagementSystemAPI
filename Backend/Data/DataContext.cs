b2bebfa0b4ebc5a1160b18d14b84a51a445482bb
shielamae02 <shiela.mlepon@gmail.com>
2023-08-27 23:19:44 +0800

    Add contact audit entity

--- Backend/Data/DataContext.cs
+++ Backend/Data/DataContext.cs
@@ -10,6 +10,7 @@ namespace Backend.Data
         public DbSet<User> Users => Set<User>();
         public DbSet<Contact> Contacts => Set<Contact>();
         public DbSet<UserAudit> UserAudits => Set<UserAudit>();
+        public DbSet<ContactAudit> ContactAudits => Set<ContactAudit>();
 
     }
 }
--- Backend/Entities/ContactAudit.cs
+++ Backend/Entities/ContactAudit.cs
@@ -1,6 +1,19 @@
-﻿namespace Backend.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Entities
 {
+    [Table("ContactAuditTrail")]
     public class ContactAudit
     {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("UserId")]
+        public int UserId { get; set; }
+        public User? User { get; set; }
+        public string Action { get; set; } = string.Empty;
+        public string Details { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
     }
 }
